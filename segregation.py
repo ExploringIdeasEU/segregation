8054e584ec850776c90cbb367f55c11287b0d83e
ExploringIdeasEU <git@exploringideas.eu>
2015-12-08 11:51:32 +0100

    skeleton

---
+++ segregation.py
@@ -0,0 +1,55 @@
+
+
+import numpy
+
+class Eveness(object):
+    def dissimilarity(x,t):
+        raise NotImplemented
+
+    def Gini():
+        raise NotImplemented
+
+    def entropy(x,t):
+        raise NotImplemented
+
+    def Atkinson():
+        raise NotImplemented
+
+class Exposure(object):
+    def interaction(x,y,t):
+        raise NotImplemented
+    def isolation(x,t):
+        raise NotImplemented
+    def correlation(x,t):
+        raise NotImplemented
+
+class Concentration(object):
+    def delta():
+        raise NotImplemented
+    def absolute():
+        raise NotImplemented
+    def relative():
+        raise NotImplemented
+
+class Centralization(object):
+    def absolute():
+        raise NotImplemented
+    def relative():
+        raise NotImplemented
+
+
+class Clustering(object):
+    def absolute():
+        raise NotImplemented
+    def relative():
+        raise NotImplemented
+    def spatial_proximity():
+        raise NotImplemented
+    def distance_decay_interaction():
+        raise NotImplemented
+    def distance_decay_isolation():
+        raise NotImplemented
+
+
+
+
