51cd87313955da777b253f5a9d270ecf23e1cd2a
ExploringIdeasEU <git@exploringideas.eu>
2015-12-11 21:12:27 +0100

    absolute and relative from Centralization group were implemented

--- segregation.py
+++ segregation.py
@@ -16,18 +16,19 @@ n    :the number of areas (census tracts) in the metropolitan area,
 m    :the number of areas (census tracts) in the metropolitan area, 
       ranked by increasing distance from the Central Business 
       District (m = n)
+
 xi   :the minority population of area i
-yi   :the majority population of area i
-yj   :the majority population of area j
-ti   :the total population of area i
-tj   :the total population of area j
 X    :the sum of all xi (the total minority population)
+yi   :the majority population of area i
 Y    :the sum of all yi (the total majority population)
+ti   :the total population of area i
 T    :the sum of all ti (the total population)
 pi   :the ratio of xi to ti (proportion of area i’s population 
       that is minority)
 P    :the ratio of X to T (proportion of the metropolitan area’s 
       population that is minority)
+ai   :the land area of area i
+A   :the sum of all ai (the total land area)
 '''
 
 import numpy
@@ -109,14 +110,51 @@ class Concentration(object):
         raise NotImplementedError()
 
 class Centralization(object):
-
+    '''Centralization is the degree to which a 
+    group is spatially located near the center
+    of an urban area.
+    Definition from:
+    http://isites.harvard.edu/fs/docs/icb.topic98848.files/massey.denton.pdf
+    '''
     @staticmethod
-    def absolute():
-        raise NotImplementedError()
+    def absolute(pair):
+        '''
+        input: pair: pair = (xi, ai, di)
+            xi: minority population in area i 
+            ai: the land area of area i 
+            di: istance from CBD of area i 
+        CDB: Central Business District 
+        '''
+        #order pair by di
+        pair = sorted(pair, key=lambda x: x[2])
+        Xi = [pair[0][0]]
+        Ai = [pair[0][1]]
+        index = 0
+        for i in range(1, len(pair)):
+            Xi.append(Xi[i-1]+pair[i][0])
+            Ai.append(Ai[i-1]+pair[i][1])
+            index += Xi[i-1]*Ai[i] - Xi[i]*Ai[-1]
+        return index / ( Xi[-1] * Ai[-1] )
 
     @staticmethod
     def relative():
-        raise NotImplementedError()
+        '''
+        input: pair: pair = (xi, yi, di)
+            xi: minority population in area i 
+            yi: majority population in area i 
+            di: istance from CBD of area i 
+        CDB: Central Business District 
+        '''
+        #order pair by di
+        pair = sorted(pair, key=lambda x: x[2])
+        Xi = [pair[0][0]]
+        Yi = [pair[0][1]]
+        index = 0
+        for i in range(1, len(pair)):
+            Xi.append(Xi[i-1]+pair[i][0])
+            Yi.append(Yi[i-1]+pair[i][1])
+            index += Xi[i-1]*Yi[i] - Xi[i]*Yi[-1]
+        return index / ( Xi[-1] * Yi[-1] )
 
 
 class Clustering(object):
